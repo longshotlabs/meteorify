2779536720b0ef8a71c872387e2d28b02d8d82d5
Eric Dobbertin <eric@dairystatedesigns.com>
2014-01-22 23:09:29 -0600

    fixes

--- meteorify.sh
+++ meteorify.sh
@@ -3,7 +3,13 @@
 CURRENT_DIR=`pwd`
 TEMP_DIR="$HOME/.meteorify/temp"
 
-mkdir -p $TEMP_DIR
-cd $TEMP_DIR
-meteor create proj "$@"
-cp -R $TEMP_DIR/proj/.meteor $CURRENT_DIR
+if [ -d ".meteor" ]; then
+    echo "This is already a Meteor app!"
+    exit 1
+fi
+
+mkdir -p $TEMP_DIR &> /dev/null
+cd $TEMP_DIR &> /dev/null
+rm -rf proj &> /dev/null
+meteor create proj "$@" &> /dev/null
+cp -R $TEMP_DIR/proj/.meteor $CURRENT_DIR &> /dev/null
